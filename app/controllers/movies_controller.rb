f0a1bc304eb7df4bca2f133a77edda9cec64eba2
Luqman <luqmannablan@gmail.com>
2024-09-11 19:08:27 -0500

    direction handling

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,34 +3,42 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    
-    sort_column = params[:sort] || 'title' #default param is to sort to title
-    sort_direction = params[:direction] || 'asc' #default param for direction is to sort ascending order
+    session[:sort_column] = params[:sort] if params[:sort].present?
+    session[:sort_direction] = params[:direction] if params[:direction].present?
+    @sort_column = session[:sort_column] || 'title' #default param is to sort to title
+    @sort_direction = session[:sort_direction] || 'asc' #default param for direction is to sort ascending order
 
-    @movies = Movie.order("#{sort_column} #{sort_direction}")
+    @movies = Movie.order("#{@sort_column} #{@sort_direction}")
 
   end
 
   # GET /movies/1 or /movies/1.json
   def show
+    @sort_column = session[:sort_column] 
+    @sort_direction = session[:sort_direction] 
   end
 
   # GET /movies/new
   def new
     @movie = Movie.new
+    @sort_column = session[:sort_column] 
+    @sort_direction = session[:sort_direction]  
   end
 
   # GET /movies/1/edit
   def edit
+    @sort_column = session[:sort_column] 
+    @sort_direction = session[:sort_direction] 
   end
 
   # POST /movies or /movies.json
   def create
     @movie = Movie.new(movie_params)
-
+    @sort_column = session[:sort_column] 
+    @sort_direction = session[:sort_direction] 
     respond_to do |format|
       if @movie.save
-        format.html { redirect_to movie_url(@movie), notice: "Movie was successfully created." }
+        format.html { redirect_to movie_path(sort: @sort_column, direction: sort_direction), notice: "Movie was successfully created." }
         format.json { render :show, status: :created, location: @movie }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -41,9 +49,12 @@ class MoviesController < ApplicationController
 
   # PATCH/PUT /movies/1 or /movies/1.json
   def update
+    @sort_column = session[:sort_column] 
+    @sort_direction = session[:sort_direction] 
+    
     respond_to do |format|
       if @movie.update(movie_params)
-        format.html { redirect_to movie_url(@movie), notice: "Movie was successfully updated." }
+        format.html { redirect_to movie_path(sort: @sort_column, direction: @sort_direction), notice: "Movie was successfully updated." }
         format.json { render :show, status: :ok, location: @movie }
       else
         format.html { render :edit, status: :unprocessable_entity }
